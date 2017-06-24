bd915896f14a6afd4625ee5929f21f0f6d35738f
acetaxxxx <acetaxxxx@gmail.com>
2017-06-24 12:22:15 +0800

    HW1 First Week

--- HomeWork/Controllers/HomeController.cs
+++ HomeWork/Controllers/HomeController.cs
@@ -3,28 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HomeWork.ViewModels;
 
 namespace HomeWork.Controllers
 {
-	public class HomeController : Controller
-	{
-		public ActionResult Index()
-		{
-			return View();
-		}
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
 
-		public ActionResult About()
-		{
-			ViewBag.Message = "Your application description page.";
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
 
-			return View();
-		}
+            return View();
+        }
 
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
 
-			return View();
-		}
-	}
+            return View();
+        }
+
+        public ActionResult BoughtList()
+        {
+            BoughtListViewModel BoutListView = ListCreator(200);
+            BoutListView.List= BoutListView.List.OrderBy(a => a.ItemDate).ToList();
+
+            return View("BoughtList", BoutListView);
+        }
+        private BoughtListViewModel ListCreator(int maxNnmber)
+        {
+            Random rnd = new Random();
+            string[] itemCategory = { "支出", "收入" };
+
+            BoughtListViewModel result = new BoughtListViewModel();
+            DateTime start = new DateTime(2016, 1, 1);
+            for (int i = 0; i < maxNnmber; i++)
+            {
+                int no = rnd.Next();
+                BoughtItem tmp = new BoughtItem()
+                {
+                    ItemCategory = itemCategory[no % 2],
+                    ItemDate = start.AddDays(no%1000).ToString("yyyy-MM-dd"),
+                    Price = no
+                };
+                result.Add(tmp);
+            }
+            return result;
+        }
+
+    }
 }
---
+++ HomeWork/ViewModels/BoughtListViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeWork.ViewModels
+{
+    public class BoughtListViewModel
+    {
+        public List<BoughtItem> List { get; set; }
+
+        public BoughtListViewModel()
+        {
+            List = new List<BoughtItem>();
+        }
+        public void Add(BoughtItem it)
+        {
+            this.List.Add(it);
+        }
+
+    }
+
+    public class BoughtItem
+    {        
+        public string ItemCategory { get; set; }
+        public string ItemDate { get; set; }
+        public int Price { get; set; }
+    }
+}
+    
